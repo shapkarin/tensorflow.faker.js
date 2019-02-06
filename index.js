aa75ce81cc50e5f368873bdf70b650a369b39d28
Yury Shapkarin <yury@shapkarin.me>
2019-02-07 00:12:38 +0300

    getTopTracksIds from musicmatch

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-import { getLyricsData } from './dataset';
+import musicmatch from 'musicmatch';
+import { apikey } from './config';
 
-async function getData(){
-    const data = await getLyricsData();
-    console.log(data);
+const music = musicmatch({ apikey });
+
+const getTopTracksIds = async function(){
+    try{
+        const { message: { body: { track_list } } } = await music.chartTracks({
+            page: 1,
+            page_size: 30,
+            country: 'ru',
+            f_has_lyrics: 1
+        });
+        const result = track_list.map(({ track }) => track.track_id);
+        return result;
+    }catch(error){
+        console.log(error);
+    }
 }
 
-getData();
+getTopTracksIds().then(ids => console.log( ids ));
